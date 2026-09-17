b1c6d774588d99aeb03ccc26369098296c2a7648
mrchudak <gleb.ts2104@gmail.com>
2026-09-17 10:51:24 +0300

    [TASK-1] Calculator

--- gleb.tsuremichev/task-1/cmd/service/main.go
+++ gleb.tsuremichev/task-1/cmd/service/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var num1 int
+	var num2 int
+	var operator string
+	_, err := fmt.Scanln(&num1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scanln(&num2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scanln(&operator)
+
+	switch operator {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 != 0 {
+			fmt.Println(num1 / num2)
+		} else {
+			fmt.Println("Division by zero")
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+
+}
